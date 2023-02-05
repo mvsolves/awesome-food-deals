67423cd93faaa2417b5a37cda7ad3b8358a511d4
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-05 14:38:32 -0600

    Imported all html and css files. Used Django tools to make static images work and made a single navbar.html that can simply be imported across all html files

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('food-deals/', views.foodDeals, name='food-deals'),
+    path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
+    path('FAQ/', views.FAQ, name='FAQ'),
 ]
 
 
--- homepage/views.py
+++ homepage/views.py
@@ -5,5 +5,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def foodDeals(request):
+    return render(request, 'food-deals.html', {})
+
+def search(request):
+    return render(request, 'search.html', {})
+
 def about(request):
     return render(request, 'about.html', {})
+
+def FAQ(request):
+    return render(request, 'FAQ.html', {})
+
+
