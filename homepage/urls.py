2ea144990481954589551cdf9595c7040522ded7
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-04 22:23:09 -0600

    Testing the waters on integrating 'index.html' and 'about.html'. Still a work in progress

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
 
 
