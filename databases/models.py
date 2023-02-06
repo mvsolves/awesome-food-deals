ad3a530ff9479053cf465115d4274f8022cca656
stephsweiss18 <122322874+stephsweiss18@users.noreply.github.com>
2023-02-06 13:57:48 -0600

    Created Relation between Restaurant and Deal Name

---
+++ databases/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Restaurant(models.Model):
+    rest_name = models.CharField(max_length=30)
+    rest_location = models.CharField(max_length=30)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
+
+    def __str__(self) -> str:
+        return self.rest_name + " at " + self.rest_location
+
+class Deal(models.Model):
+    deal_name = models.CharField(max_length=30)
+    rest = models.ManyToManyField(Restaurant)
+
+    def __str__(self) -> str:
+        return self.deal_name
