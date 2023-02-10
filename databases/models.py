04f2c0e2e4b44cb2df41ff1bd0bf22356c9cb4b5
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-09 19:33:56 -0600

    Fix: Users can now add more than one restaurant
    OneToOne field changed to ManyToMany field

--- databases/models.py
+++ databases/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Restaurant(models.Model):
     rest_name = models.CharField(max_length=30)
     rest_location = models.CharField(max_length=30)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
+    user = models.ManyToManyField(User, primary_key=False)
 
     def __str__(self) -> str:
         return self.rest_name + " at " + self.rest_location
