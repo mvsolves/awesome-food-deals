61c66a90678b8a72474cb05d1062ab7a536bc843
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-12 14:38:31 -0600

    Fix many-to-many between deals and restaurants

--- databases/models.py
+++ databases/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Deal(models.Model):
+    deal_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.deal_name
+
 class Restaurant(models.Model):
+    deals = models.ManyToManyField(Deal, related_name="deals")
     rest_name = models.CharField(max_length=30)
     rest_location = models.CharField(max_length=30)
     user = models.ManyToManyField(User, primary_key=False)
 
     def __str__(self) -> str:
         return self.rest_name + " at " + self.rest_location
-
-class Deal(models.Model):
-    deal_name = models.CharField(max_length=30)
-    rest = models.ManyToManyField(Restaurant, related_name='restaurant_deals')
-
-    def __str__(self) -> str:
-        return self.deal_name
