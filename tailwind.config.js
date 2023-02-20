da0df7af4a5a8b7b485dbd379cfa57f1a8d622be
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-20 16:04:36 -0600

    Update home page. Make login page work
    This commit add a ton of minor details on how everything is laid out.
    The login feature works. When correct user is logged in, it redirects
    them back to home page. If it doesnt, an error pops up while still being
    in the login page. Also, added tailwind configs to find the login.html.
    That was giving me major issues at first.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./homepage/templates/*.{html,js}'],
+  content: ['./homepage/templates/*.{html,js}',
+            './databases/templates/authenticate/*.{html,js}'],
   theme: {
     extend: {},
   },
