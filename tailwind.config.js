efde305c70de04db165c5852e80eebf0352c0e73
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-21 16:29:19 -0600

    Add default tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ['./homepage/templates/*.{html,js}',
-            './databases/templates/authenticate/*.{html,js}'],
+  content: ['./homepage/templates/*.{html,htmldjango,js}',
+            './databases/templates/authenticate/*.{html,js,htmldjango}'],
   theme: {
     extend: {},
   },
