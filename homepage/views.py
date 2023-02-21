56a2f94c9535491e7a0a2bab53dce37274543232
stephsweiss18 <122322874+stephsweiss18@users.noreply.github.com>
2023-02-20 23:31:51 -0600

    Show favorited deals on favorites page

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from databases.models import Restaurant, Deal
 
-from databases.models import Restaurant
-
 # Create your views here.
 
 def index(request):
@@ -21,7 +19,13 @@ def search(request):
     return render(request, 'search.html', {})
 
 def favorites(request):
-    return render(request, 'favorites.html', {})
+    count = Restaurant.objects.count()
+    allRestaurants = Restaurant.objects.all()
+    allDeals= Deal.objects.all().values()
+    return render(request, 'favorites.html', {'varAllRestaurants': allRestaurants,
+                                               'varAllDeals': allDeals,
+                                               'totalCount': count
+                                               })
 
 def about(request):
     return render(request, 'about.html', {})
