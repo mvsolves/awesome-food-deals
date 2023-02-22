d623812c107abfa0c7ac61d8190558a52cc7b435
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-22 13:42:50 -0600

    Update favorites tab to display favorited restaurants

--- databases/models.py
+++ databases/models.py
@@ -12,7 +12,6 @@ class Customer(models.Model):
 # Create your models here.
 class Deal(models.Model):
     deal_name = models.CharField(max_length=30)
-    favorite = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.deal_name
@@ -22,6 +21,7 @@ class Restaurant(models.Model):
     deals = models.ManyToManyField(Deal, related_name="deals")
     rest_name = models.CharField(max_length=30)
     rest_location = models.CharField(max_length=30)
+    favorite = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.rest_name + " at " + self.rest_location
