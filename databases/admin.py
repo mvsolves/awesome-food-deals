99fd3a1dc2b667bd4dbc245c745eeeb9e3f62a74
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-27 15:51:31 -0600

    Add new model, 'Location'

--- databases/admin.py
+++ databases/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Restaurant, Deal, Customer
+from .models import Restaurant, Deal, Customer, Location
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Deal)
 admin.site.register(Customer)
+admin.site.register(Location)
--- databases/models.py
+++ databases/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -9,19 +10,23 @@ class Customer(models.Model):
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
 
-# Create your models here.
 class Deal(models.Model):
     deal_name = models.CharField(max_length=30)
 
     def __str__(self) -> str:
         return self.deal_name
 
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.location_name
 
 class Restaurant(models.Model):
     deals = models.ManyToManyField(Deal, related_name="deals")
     rest_name = models.CharField(max_length=30)
-    rest_location = models.CharField(max_length=30)
+    rest_location = models.ForeignKey('Location', null=True, on_delete=models.CASCADE)
     favorite = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return self.rest_name + " at " + self.rest_location
+        return self.rest_name
