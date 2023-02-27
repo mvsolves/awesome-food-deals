b26e8586297bdb21df4441be117f5b2fcdcbb00f
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-27 17:40:48 -0600

    Add simple filter system

--- homepage/views.py
+++ homepage/views.py
@@ -6,15 +6,32 @@ from databases.models import Restaurant, Deal, Location
 def index(request):
     return render(request, 'index.html', {})
 
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
 def foodDeals(request):
     count = Restaurant.objects.count()
     allRestaurants = Restaurant.objects.all()
     allDeals= Deal.objects.all().values()
     allLocations = Location.objects.all().values()
+
+    loc = request.GET.get('loc')
+    deal = request.GET.get('deal')
+
+    print(loc)
+    print(deal)
+
+    if is_valid_queryparam(loc):
+        allRestaurants = allRestaurants.filter(rest_location__location_name = loc)
+
+    if is_valid_queryparam(deal):
+        allRestaurants = allRestaurants.filter(deals__deal_name = deal)
+
     return render(request, 'food-deals.html', {'varAllRestaurants': allRestaurants,
                                                'varAllDeals': allDeals,
                                                'varAllLocations': allLocations,
-                                               'totalCount': count
+                                               'varTotalCount': count,
                                                })
 def favorites(request):
     count = Restaurant.objects.count()
