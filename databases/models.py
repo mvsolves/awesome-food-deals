ecb138716d2d7ea9b951e026b942974637c70b1a
Marcos Velazquez <mvelaz24@depaul.edu>
2023-02-27 20:49:42 -0600

    Fix favorites tab to work for Customers
    Before, the favorites tab was showing a general favorites, regardless if
    a user was logged in or not. This is NOT what we wanted.

--- databases/models.py
+++ databases/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
-class Customer(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
-
-    def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
-
 class Deal(models.Model):
     deal_name = models.CharField(max_length=30)
 
@@ -26,7 +20,24 @@ class Restaurant(models.Model):
     deals = models.ManyToManyField(Deal, related_name="deals")
     rest_name = models.CharField(max_length=30)
     rest_location = models.ForeignKey('Location', null=True, on_delete=models.CASCADE)
-    favorite = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.rest_name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
+    favorite_rest = models.ManyToManyField(Restaurant, related_name="restaurants")
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customer.save()
+
